754daffdb7a75ad2e6f3b0250ddf72d019790cd4
ercelserhat <ercelserhat@gmail.com>
2023-07-14 14:02:51 +0300

    Basic definitions

--- KisilerApp/ViewController.swift
+++ KisilerApp/ViewController.swift
@@ -6,18 +6,81 @@
 //
 
 import UIKit
+import CoreData
+
+let appDelegate = UIApplication.shared.delegate as! AppDelegate
 
 class ViewController: UIViewController {
+    
+    let context = appDelegate.persistentContainer.viewContext
 
     @IBOutlet weak var searchBar: UISearchBar!
     
     @IBOutlet weak var kisilerTableView: UITableView!
     
+    var kisilerListe = [Kisiler]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        kisilerTableView.dataSource = self
+        kisilerTableView.delegate = self
+        searchBar.delegate = self
+        tumKisileriAl()
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toKisiDetay"{
+            
+        }
+        if segue.identifier == "toKisiGuncelle"{
+            
+        }
+    }
+    
+    func tumKisileriAl(){
+        do{
+            kisilerListe = try context.fetch(Kisiler.fetchRequest())
+        }catch{
+            print(error)
+        }
+    }
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return kisilerListe.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let kisi = kisilerListe[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "kisiHucre", for: indexPath) as! KisiHucreTableViewCell
+        cell.kisiYaziLabel.text = "\(kisi.kisi_ad!) - \(kisi.kisi_tel!)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "toKisiDetay", sender: indexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let silAction = UIContextualAction(style: .destructive, title: "Sil") { contextualaction, view, boolValue in
+        
+        }
+        let guncelleAction = UIContextualAction(style: .normal, title: "Güncelle") { contextualaction, view, boolValue in
+            self.performSegue(withIdentifier: "toKisiGuncelle", sender: indexPath.row)
+        }
+        return UISwipeActionsConfiguration(actions: [silAction, guncelleAction])
+    }
+}
 
+extension ViewController: UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("Arama sonuç: \(searchText)")
+    }
 }
 
